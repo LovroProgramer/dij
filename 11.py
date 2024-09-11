40458bf44f9057998c28e88b39be5c6cf936722e
LovroProgramer <49488967+LovroProgramer@users.noreply.github.com>
2024-09-11 13:22:35 +0200

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,6 +1,6 @@
 def brojPutova(g, s, d, visited):
     if s == d:
-        return 1  # Ako smo došli do ciljnog čvora, to je jedan put
+        return 1  
     visited.add(s)
     count = 0
     for susjed in g[s]:
@@ -9,15 +9,14 @@ def brojPutova(g, s, d, visited):
     visited.remove(s)
     return count
 
-# Funkcija za pronalaženje broja alternativnih putova
+
 def brojAlternativnihPutova(g, s, d):
-    visited = set()  # Skup posjećenih čvorova
+    visited = set()  
     broj_putova = brojPutova(g, s, d, visited)
     if broj_putova <= 1:
         return "Nema alternativnih putova."
     else:
         return f"Broj alternativnih putova: {broj_putova - 1}"
 
-# Testiranje funkcije
 broj_alt_putova = brojAlternativnihPutova(g, 1, 10)
 print(broj_alt_putova)
